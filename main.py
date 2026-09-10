111d25b4c31fb347e63f767e124038187366ecf9
josephmolina256 <josephmolina256@gmail.com>
2026-09-10 12:42:50 -0400

    phase 2

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
+import fastapi
 
-query = "a story about someone discovering their reality is fake"
-texts = [
-    "A scientist discovers humanity lives inside a computer simulation.",
-    "A detective investigates a mysterious murder in New York.",
-    "A young chef learns to create a new recipe for a restaurant.",
-    "A person realizes their life is controlled by an artificial world."
-]
+app = fastapi.FastAPI()
 
-model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
-query_embedding = model.encode([query]) # [dndd, dh]
-text_embeddings = model.encode(texts) # [[dnndd], [dejde]]
+@app.get("/search")
+def search(query: str):
+    texts = [
+        "A scientist discovers humanity lives inside a computer simulation.",
+        "A detective investigates a mysterious murder in New York.",
+        "A young chef learns to create a new recipe for a restaurant.",
+        "A person realizes their life is controlled by an artificial world."
+    ]
 
-scores = cosine_similarity(query_embedding, text_embeddings)[0]
-ranked = sorted(zip(texts, scores), key=lambda item: item[1], reverse=True)
+    model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+    query_embedding = model.encode([query]) # [dndd, dh]
+    text_embeddings = model.encode(texts) # [[dnndd], [dejde]]
 
-print(f"Query: {query}\n")
-for i, (text, score) in enumerate(ranked, start=1):
-    print(f"{i}. Score: {score:.4f} | {text}")
+    scores = cosine_similarity(query_embedding, text_embeddings)[0]
+    ranked = sorted(zip(texts, scores), key=lambda item: item[1], reverse=True)
+
+    print(f"Query: {query}\n")
+    res = []
+    for i, (text, score) in enumerate(ranked, start=1):
+        print(f"{i}. Score: {score:.4f} | {text}")
+        res.append({"text": text, "score": float(score)})
+
+    return res
 
 
